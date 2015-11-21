0f2157b3accfcb6efa7fd6f6bf37dde8445811e4
gbchoi <mosfet1kg@gmail.com>
2015-11-21 16:49:47 +0900

    update

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 var express = require('express'),
     fs = require('fs'),
     path = require('path'),
-    app = express();
+    app = express(),
+    engine = require('ejs-mate'),
+    bodyParser = require('body-parser');
+
+app.use( express.static( path.join(__dirname, 'public' ) ) )
+   .use(bodyParser.urlencoded({ extended: false }))  // parse application/x-www-form-urlencoded
+   .use(bodyParser.json());                          // parse application/json
+
+// use ejs-locals for all ejs templates:
+app.engine('ejs', engine);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'ejs'); // so you can render('index')
 
 
 app.get('/', function(req, res){
@@ -10,12 +21,11 @@ app.get('/', function(req, res){
 
 app.get('/get/*', function(req,res){
 
-    var tmp_file = '',
-        tmp_folder = '',
-        ids = req.params,
+    var ids = req.params,
         dir = '',
         shared_folder = path.join(__dirname, 'shared'),
-        list = {};
+        list_folders = [],
+        list_files = [];
 
     dir = path.join( ids[0] );
 
@@ -27,20 +37,21 @@ app.get('/get/*', function(req,res){
             console.log(err);
             res.error({err:err.message});
         }else{
-            var parent = dir != '.' ?  '<a href="/get/' +  path.normalize(path.join(dir,'..'))+'">[..]</a><br/>' : '';
+            if( dir != '.' )
+                list_folders.push({isFolder: true, link: path.normalize( path.join(dir,'..') ), name: '..'});
 
             for(var i in files){
                 var file = files[i];
 
                 var stats = fs.statSync(path.join(shared_folder, dir, file) );
                 if(stats.isFile()){
-                    tmp_file += '<a href="/down/'+path.join(dir,file)+'" download>'+file+'</a><br/>';
+                    list_files.push( {isFolder: false, link: path.join(dir,file), name: file } );
                 }else{
-                    tmp_folder +='<a href="/get/'+path.join(dir,file)+'">['+file+']</a><br/>';
+                    list_folders.push( {isFolder: true, link:path.join(dir,file), name: file })
                 }
             }
 
-            res.send(parent + tmp_folder + tmp_file );
+            res.render('explore', { list: list_folders.concat(list_files) } );
         }
     });
 });
@@ -53,7 +64,29 @@ app.get('/down/*', function(req, res){
     res.sendFile( path.join( shared_folder,  file_path ));
 });
 
+app.delete('/remove', function(req, res){
+    var shared_folder = path.join(__dirname, 'shared'),
+        target = path.join( shared_folder, req.body.item );
 
+    var stats = fs.statSync( target );
+    if(stats.isFile()){
+        fs.unlink(target, function(err){
+            if(!!err){
+                res.error({success: false})
+            }
+            res.json({success: true});
+        } )
+    }else{
+        fs.rmdir( target, function(err){
+            if(!!err){
+                res.error({success: false})
+            }
+            res.json({success: true});
+        } )
+    }
+
+    //fs.unlink()
+});
 app.listen(55555, function(){
     console.log('This server server is running on the port ' + this.address().port);
 });
